5eb9901ce186d54c3c171dccc94479da07e857fc
samsaem <samhpham@ucla.edu>
2024-02-13 11:14:37 -0800

    miscellaneous

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="AmericanIPAChart"
+          href="https://hlw.id.ucsb.edu/toolkit/ipaTable/index.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          American IPA Chart
         </a>
       </header>
     </div>
