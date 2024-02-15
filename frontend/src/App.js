701a10b2ba150b853aef2fba5c06320e0647472f
samsaem <samhpham@ucla.edu>
2024-02-15 12:27:18 -0800

    *blank*

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="AmericanIPAChart"
-          href="https://hlw.id.ucsb.edu/toolkit/ipaTable/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          American IPA Chart
-        </a>
-      </header>
+
     </div>
   );
 }
