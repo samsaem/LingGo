0f27e8dbe99f7bf4b589071bddeb7a7ce0c63725
ccastillo5229 <105951696+ccastillo5229@users.noreply.github.com>
2024-02-27 11:23:03 -0800

    Revert "Revert "Added Consonants""
    This reverts commit 1e69502043e04ce34b61c988693adeded9b75cc0.

---
+++ frontend/src/IPA_chart.js
@@ -0,0 +1,170 @@
+import './IPA_chart.css';
+
+
+
+
+
+
+
+function IPA_chart() {
+
+
+  return (
+  <>
+  <table>
+    <caption>
+      <h2>English Consonants</h2>
+    </caption>
+    <tr>
+      <th></th>
+      <th colspan="2">Bilabial</th>
+      <th colspan ="2">Labiodental</th>
+      <th colspan ="2">Dental</th>
+      <th colspan ="2">Alveolar</th>
+      <th colspan ="2">Postalveolar</th>
+      <th colspan ="2">Palatal</th>
+      <th colspan ="2">Velar</th>
+      <th colspan ="2">Glotal</th>
+    </tr>
+    <tr>
+      <td>Plosive</td>
+      <td>p</td>
+      <td>b</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>t</td>
+      <td>d</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>k</td>
+      <td>g</td>
+      <td>ʔ</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Nasal</td>
+      <td></td>
+      <td>m</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>n</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>ŋ</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Tap or Flap</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>ɾ</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Affricative</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>tʃ</td>
+      <td>dʒ</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Fricative</td>
+      <td></td>
+      <td></td>
+      <td>f</td>
+      <td>v</td>
+      <td>θ</td>
+      <td>ð</td>
+      <td>s</td>
+      <td>z</td>
+      <td>ʃ</td>
+      <td>ʒ</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>h</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Approximant</td>
+      <td></td>
+      <td>w</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>ɹ</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>j</td>
+      <td></td>
+      <td>w</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Lateral Approximant</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>l</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+  <small>
+    The symbols to the right are voiced, left are voiceless.
+  </small>
+  </>
+  );
+}
+
+export default IPA_chart;
