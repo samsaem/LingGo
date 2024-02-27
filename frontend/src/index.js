68f27c770c191e5b4eca0e5e4ffd7e5d1c62dc37
ccastillo5229 <charles@LAPTOP-OI129JMC>
2024-02-27 10:34:55 -0800

    Added Consonants
    Added IPA chart for English Consonants

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import IPA_chart from './IPA_chart';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <IPA_chart />
   </React.StrictMode>
 );
 
