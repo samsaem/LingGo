43c2697850087f6e72854a6a3bff74b7b9faf96b
Cavezzo <cvezzo@hotmail.com>
2024-03-05 00:23:53 -0800

    Display notes and add pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -2,34 +2,48 @@ const Note = require('../models/Notes');
 const mongoose = require('mongoose');
 
 
-
-
 // Dashboard
 exports.dashboard = async (req, res) => {
+
   const locals = {
       title: "Dashboard",
       description: "Interactive IPA web app!",
   }
 
+  let perPage = 12;
+  let page = req.query.page || 1;
+
+
   try {
-    const notes = await Note.find({});
-    
+ 
+    const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 200] },
+        },
+      }
+      ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(); 
+
+    const count = await Note.countDocuments();
+
     res.render('dashboard/index', {
-      userName: req.user.firstName, 
+      userName: req.user.firstName,
       locals,
-      notes, 
-      layout: '../views/layouts/dashboard'
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage)
     });
 
-  } catch (error){
-
-  }
-
-
-
-
-
-
+} catch (error) {
+  console.log(error);
+}
 
 
 }
